580f46b3b4e56ad00d3008cd340df738cea012aa
haimsingh <41017415+haimsingh@users.noreply.github.com>
2018-10-06 15:45:43 -0400

    Completed challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,25 @@
 // Example
-let message = "Hello World!";
+const READLINE = require('readline-sync')
+let message = "Video Game File: SKEli";
 console.log(message);
 
 // Type your code below!
+let SmashWins = "2,798 Wins"
+console.log(SmashWins) 
+let SmashLosses = "1,269 Losses"
+console.log(SmashLosses)
+let FirstMain = "First Main: Sonic"
+console.log(FirstMain)
+let SecondMain = "Second Main; Cloud"
+console.log (SecondMain)
+ let FavoriteGame = "Sonic Generations"
+let Numberofconsoles= 5
+let NumberofSpidermangames= 3
+let Numberofsports= 4
+
+ SmashWins= true
+ SmashLosses=false
+ FirstMain= true
+
+ let userInput = READLINE.question("Type anything and I'll repeat it back!");
+ console.log(userInput)
