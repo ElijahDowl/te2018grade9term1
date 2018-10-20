ad8462e18d75fdb7713403ce167f7daedb27b9d6
haimsingh <41017415+haimsingh@users.noreply.github.com>
2018-10-20 16:03:19 -0400

    Almost finished change machine

--- change.js
+++ change.js
@@ -2,6 +2,29 @@
 const READLINE = require("readline-sync");
 
 // fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
+let changeMachine = {
+	inputCents:undefined,
+	quarters:undefined,
+	dimes:undefined,
+	nickels:undefined,
+	pennies:undefined,
+	greeting:"Hello! How much do you want to withdraw?",
+	farewell:"Thank You! Please come again!"
+};
 
 // write code below that simulates the change machine in action!
+let inputCents=READLINE.question("Hello! How much do you want to withdraw?:  ");
+let quarters = Math.floor(inputCents/25);
+let dimes = Math.floor((inputCents%25)/10);
+let nickels = Math.floor(((inputCents%25)%10)/5);
+let pennies  = Math.floor(((inputCents%25)%10)%5);
+
+changeMachine.inputCents= inputCents
+changeMachine.quarters= quarters
+changeMachine.dimes= dimes
+changeMachine.nickels= nickels
+changeMachine.pennies= pennies
+
+console.log(`${changeMachine.inputCents} makes:
+
+	)
---
+++ objects.js
@@ -0,0 +1,21 @@
+let Nemesis;
+Nemesis = {
+	firstName: "Ivan",
+	lastName: "Sanders",
+	nickname: "Agent 096",
+	catchphrase: "Boom Goes the Dynamite",
+	greatestCrime: "Blowing up the whole city"
+}
+
+let Hoodie;
+Hoodie = {
+	firstName: "Hunter",
+	lastName: "James",
+	nickname: "Agent 015",
+	catchphrase: "Let's Go",
+	archenemy: "Ivan Sanders"
+}
+
+console.log (`Hi, I'm Eli- I mean ${Hoodie.firstName}.`);
+console.log (`My Agent Number is ${Hoodie.nickname}`);
+console.log (`My archenemy is ${Hoodie.archenemy}`);
