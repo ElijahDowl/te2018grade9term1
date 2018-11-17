c56ca761e3929304dfd836a73f673699b81733a9
ElijahDowl <43911395+ElijahDowl@users.noreply.github.com>
2018-11-17 15:51:44 -0500

    finished haunted

--- haunted.js
+++ haunted.js
@@ -9,24 +9,40 @@ let player = {
   weapon: "vacuum cleaner",
   item: "flashlight",
 };
-
+let death = (`${player.name} turns around and sees a bat hit their face. The last thing they hear before dying is 'I told you Grandpa would find you'`)
 console.log();
 console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
 player.name = READLINE.question("What is your name?: ");
 console.log(`${player.name} stands in front of a spooky haunted house they were hired to clean`);
 console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
 let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
+
 if(enterHouse == "y" || enterHouse == "yes") {
   player.courage++;
   console.log(`${player.name} prepares their ${player.weapon} and opens the door is greeted by a little boy holding a little red ball. He seems harmless enough. Until ${player.name} looks more closely... Th-Thats not a ball.. thats a severed head!! 'Grandpa's Gonna find yoooouuuu hehehehe' Then the boy runs off intyo the darkness of the house`);
  let leaveOrStay =  READLINE.question("Do you leave the house?") 
- console.log(`Boom!!! Click!! Thre door slams shut and locks, trapping ${player.name} inside. 'Aw Crap' ${player.name} exclaims. 'I have to find a way out' ${player.name} looks for a window, but soon realises that the house has no windows. Then ${player.name} hears a 'THUNK' 'THUNK' 'THUNK'. ${player.name} turns around and sees a bat hit their face. The last thing they hear before dying is 'I told you Grandpa would find you' `); 
-
-
-} else {
-  player.intellect++;
-  console.log(`${player.name} decides not to open the door. However they hear a groan. It seems like it's coming from the ground. Is that a hand? ZOMBIES!! The undead creatures rise from the ground and starts chasing ${player.name}. They decide${player.name} goes home and never comes back. `);
-  // continue the story
-
+ console.log(` SLAM!!! Click!! Thre door slams shut and locks, trapping ${player.name} inside. 'Aw Crap' ${player.name} exclaims. 'I have to find a way out' ${player.name} looks for a window, but soon realizes that the house has no windows. Then ${player.name} hears a 'THUNK' 'THUNK' 'THUNK'. That kid's grandpa is coming!!! What to do what to do!!! Think ${player.name} `); 
+ 
+ let hideOrRun = READLINE.question('Will you run? Or will you hide?');
+ if(hideOrRun == "run"){
+ 
+console.log(`You decide to run. ${player.name} sprint for the door.'Damn! its locked, I forgot!' ${player.name} turns around and sees a bat hit their face. The last thing they hear before dying is 'I told you Grandpa would find you' `);
 }
-console.log("Thanks for playing!");
+ else{ 
+  console.log(`You decide to hide under the couch. The Kid's grandpa comes into the room, but hasn't found you. He unlocks the door and looks outside for you. after coming inside, he forgets to lock the door. ${player.name} absolutely books it out of the house and doesnt look back`)
+ }
+}
+ else{
+  player.intellect++;
+  console.log(`${player.name} Despite their better judgement prepares their ${player.weapon} and opens the door is greeted by a little boy holding a little red ball. He seems harmless enough. Until ${player.name} looks more closely... Th-Thats not a ball.. thats a severed head!! 'Grandpa's Gonna find yoooouuuu hehehehe' Then the boy runs off intyo the darkness of the house`);
+ let leaveOrStay =  READLINE.question("Do you leave the house?") 
+ console.log(`Boom!!! Click!! Thre door slams shut and locks, trapping ${player.name} inside. 'Aw Crap' ${player.name} exclaims. 'I have to find a way out' ${player.name} looks for a window, but soon realizes that the house has no windows. Then ${player.name} hears a 'THUNK' 'THUNK' 'THUNK'. That kid's grandpa is coming!!! What to do what to do!!! Think ${player.name} `); 
+ }
+ let hideOrRun = READLINE.question('Will you run? Or will you hide?');
+ if(hideOrRun == "run"){
+console.log(`You decide to run. ${player.name} sprint for the door.'Damn! its locked, I forgot!' ${player.name} turns around and sees a bat hit their face. The last thing they hear before dying is 'I told you Grandpa would find you' `);
+}
+ else{
+  console.log(`You decide to hide under the couch. The Kid's grandpa comes into the room, but hasn't found you. He unlocks the door and looks outside for you. after coming inside, he forgets to lock the door. ${player.name} absolutely books it out of the house and doesnt look back`)
+}
+console.log("Thanks for playing!")
